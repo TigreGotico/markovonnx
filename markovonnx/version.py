d499a5f5f0f533d85984a19607fa47ba4f9f5d00
JarbasAl <33701864+JarbasAl@users.noreply.github.com>
2026-05-21 15:40:13 +0000

    Increment Version to 0.0.1a1

--- markovonnx/version.py
+++ markovonnx/version.py
@@ -4,8 +4,8 @@ __version__ = "0.3.0"
 # START_VERSION_BLOCK
 VERSION_MAJOR = 0
 VERSION_MINOR = 0
-VERSION_BUILD = 0
-VERSION_ALPHA = 0
+VERSION_BUILD = 1
+VERSION_ALPHA = 1
 # END_VERSION_BLOCK"""Version information for markovonnx."""
 
 __version__ = "0.3.0"
